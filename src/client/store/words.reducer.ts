0634a91ca8c6856a0ac5f80ac7edb9c3522af19e
Guillermo Loaysa <gloaysa@gmail.com>
2023-03-10 11:31:48 +0100

    fix(): after adding word, FE checks if exists

--- src/client/store/words.reducer.ts
+++ src/client/store/words.reducer.ts
@@ -1,42 +1,49 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit'
-import {RootState} from './store';
-import {IWord} from '../../common/interfaces/word.interface';
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "./store";
+import {IWord} from "../../common/interfaces/word.interface";
 
 // Define a type for the slice state
 interface WordsState {
-    wordList: IWord[];
-    currentWord: string;
-    currentWordExist: boolean;
+  wordList: IWord[];
+  currentWord: string;
+  currentWordExist: boolean;
 }
 
 // Define the initial state using that type
 const initialState: WordsState = {
-    wordList: [],
-    currentWord: '',
-    currentWordExist: false,
-}
+  wordList: [],
+  currentWord: "",
+  currentWordExist: false,
+};
 
 export const wordsSlice = createSlice({
-    name: 'letters',
-    initialState,
-    reducers: {
-        setWordList: (state, action: PayloadAction<IWord[]>) => {
-            state.wordList = action.payload;
-        },
-        setNewWord: (state, action: PayloadAction<IWord>) => {
-          state.wordList = [...state.wordList, action.payload];
-          state.currentWordExist = state.currentWord === action.payload.value;
-        },
-        setCurrentWord: (state, action: PayloadAction<string>) => {
-            state.currentWord = action.payload;
-            state.currentWordExist = !!state.wordList.some(({value}) => value === action.payload);
-        }
+  name: "letters",
+  initialState,
+  reducers: {
+    setWordList: (state, action: PayloadAction<IWord[]>) => {
+      state.wordList = action.payload;
     },
-})
+    setNewWord: (state, action: PayloadAction<IWord>) => {
+      const newWordList = [...state.wordList, action.payload];
+      state.wordList = newWordList;
+      state.currentWordExist = wordExist(newWordList, action.payload.value);
+    },
+    setCurrentWord: (state, action: PayloadAction<string>) => {
+      state.currentWord = action.payload;
+      state.currentWordExist = wordExist(state.wordList, action.payload);
+    },
+  },
+});
 
-export const { setWordList, setCurrentWord, setNewWord } = wordsSlice.actions
+const wordExist = (wordList: IWord[], word: string): boolean => {
+  return wordList.some((existingWord) =>
+    existingWord.allForms.some((form) => form === word)
+  );
+};
+
+export const { setWordList, setCurrentWord, setNewWord } = wordsSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectWordState = (state: RootState): WordsState => state.words;
 
-export default wordsSlice.reducer
+export default wordsSlice.reducer;
