1e66ab6eb1954e24e340bd3cd3085af8af2a3f03
Guillermo Loaysa <gloaysa@gmail.com>
2023-03-11 18:26:34 +0100

    feat(): add app icons

--- webpack.config.js
+++ webpack.config.js
@@ -7,64 +7,58 @@ const CopyPlugin = require('copy-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.tsx'],
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: './js/[name].bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader'
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader'
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.ts', '.tsx', '.js', '.jsx', '.json']
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-    proxy: {
-      '/api/**': {
-        target: 'http://localhost:8050',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico',
-    }),
-    new MiniCssExtractPlugin(),
-    new CopyPlugin([
-      {from: './src/client/assets', to: 'assets'},
-    ])
-  ],
+	entry: ['babel-polyfill', './src/client/index.tsx'],
+	output: {
+		path: path.join(__dirname, outputDirectory),
+		filename: './js/[name].bundle.js',
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+				loader: 'url-loader',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.ts', '.tsx', '.js', '.jsx', '.json'],
+	},
+	devServer: {
+		port: 3000,
+		open: true,
+		hot: true,
+		proxy: {
+			'/api/**': {
+				target: 'http://localhost:8050',
+				secure: false,
+				changeOrigin: true,
+			},
+		},
+	},
+	plugins: [
+		new CleanWebpackPlugin([outputDirectory]),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			favicon: './public/icons/favicon.ico',
+		}),
+		new MiniCssExtractPlugin(),
+		new CopyPlugin([{ from: './src/client/assets', to: 'assets' }]),
+	],
 };
