e3d3bbca0415e4570ed2937ed08858a91cd315bb
Guillermo Loaysa <gloaysa@gmail.com>
2023-04-08 13:41:36 +0200

    feat(): close menu on selecting item

--- src/client/components/hambuger-menu/hamburger-menu.component.tsx
+++ src/client/components/hambuger-menu/hamburger-menu.component.tsx
@@ -1,24 +1,35 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import './hamburger-menu.component.scss';
 
+interface IHamburgerItem {
+	label: string;
+	callback?: () => void;
+	link?: string;
+}
+
 interface IHamburgerMenuComponent {
-	linkList: {
-		label: string;
-		callback?: () => void;
-		link?: string;
-	}[];
+	linkList: IHamburgerItem[];
 }
 const HamburgerMenuComponent: FunctionComponent<IHamburgerMenuComponent> = ({ linkList }) => {
+	const [checked, setChecked] = useState(false);
+
+	const handleHamburgerClick = (item?: IHamburgerItem) => {
+		setChecked(!checked);
+		if (item?.callback) {
+			item.callback();
+		}
+	};
+
 	return (
 		<nav role="navigation">
 			<div id="menuToggle">
-				<input type="checkbox" />
+				<input type="checkbox" checked={checked} onChange={() => handleHamburgerClick()} />
 				<span></span>
 				<span></span>
 				<span></span>
 				<ul id="menu">
 					{linkList.map((item) => (
-						<a key={item.label} onClick={item.callback}>
+						<a key={item.label} onClick={() => handleHamburgerClick(item)}>
 							<li>{item.label}</li>
 						</a>
 					))}
